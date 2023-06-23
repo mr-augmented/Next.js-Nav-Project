3fdce0c221838a09210bae0c0ab40c6e5ad7d414
Jaylan Thompson <jaylanthompson@Jaylans-MacBook-Pro.local>
2023-06-23 15:29:07 -0400

    Moved files to pages directory

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from "/Users/jaylanthompson/Desktop/My Desktop/PROGRAMMING FOLDER/js-folder/components/Navbar.js";
+
+function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Pillar</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import Navbar from "/Users/jaylanthompson/Desktop/My Desktop/PROGRAMMING FOLDER/js-folder/components/Navbar.js";
+
+function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Pillar</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
